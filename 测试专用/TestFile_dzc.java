129e7087a2d1930d951b4d1b165843629900dd72
Aandrew Han <woshifashi117@163.com>
2018-10-31 14:16:12 +0800

    Change on TestFile_dzc.java

--- 测试专用/TestFile_dzc.java
+++ 测试专用/TestFile_dzc.java
@@ -1,5 +1,6 @@
 public class TestFile_dzc{
 	public static void main(String[] args){
 	System.out.println("Members: dzc, wsn, shx, tiger, wyt, hyd");
+	System.out.println("Andrew said: I change first!");
 }
 }
